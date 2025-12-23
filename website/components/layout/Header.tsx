a0926360f86dfa9d0a8b565457203651effd4ac6
Pipe <fgbravo.m@gmail.com>
2025-12-23 18:40:05 -0300

    cambios al top bar

--- website/components/layout/Header.tsx
+++ website/components/layout/Header.tsx
@@ -35,7 +35,7 @@ export function Header() {
       scrolled ? 'bg-azul-primario shadow-lg' : 'bg-azul-primario md:bg-transparent'
     }`}>
       <div className="container mx-auto px-2 md:px-3 lg:px-4">
-        <div className="flex h-18 md:h-16 lg:h-20 items-center justify-between gap-1 md:gap-2 lg:gap-4">
+        <div className="flex h-24 md:h-16 lg:h-20 items-center justify-between gap-1 md:gap-2 lg:gap-4">
           {/* Mobile Menu Button - Left on mobile */}
           <button
             className="md:hidden p-2 text-white order-1"
@@ -73,7 +73,7 @@ export function Header() {
               alt={`Escudo ${SITE_CONFIG.name}`}
               width={100}
               height={100}
-              className="h-16 md:h-11 lg:h-16 w-auto"
+              className="h-20 md:h-11 lg:h-16 w-auto"
             />
           </Link>
 
